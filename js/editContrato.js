a2c5e177a6bb59d3ed701fc697912dac33827c57
Daniel-GM <danielgomesmoura@gmail.com>
2023-07-31 10:41:29 -0300

    contrato dinamico

---
+++ js/editContrato.js
@@ -0,0 +1,48 @@
+const btnPdf = document.querySelector('#generate-pdf')
+
+const nomeFantasia = document.querySelector('#input-nome')
+const empresaContratante = document.querySelector('#input-nome')
+const cnpj = document.querySelector('#input-cnpj')
+const inscricaoEstadual = document.querySelector('#input-IE')
+const rua = document.querySelector('#input-rua')
+const numeroRua = document.querySelector('#input-numero-rua')
+const bairro = document.querySelector('#input-bairro')
+const cidade = document.querySelector('#input-cidade')
+const cep = document.querySelector('#input-cep')
+const representante = document.querySelector('#input-representante')
+const cidadania = document.querySelector('#input-cidadania')
+const estadoCivil = document.querySelector('#input-civil')
+const Profissao = document.querySelector('#input-profissao')
+const cpf = document.querySelector('#input-cpf')
+const rg = document.querySelector('#input-rg')
+
+let data = new Date() /* gerado pelo sistema */
+let dia = String(data.getDate()).padStart(2, '0')
+let mes = String(data.getMonth() + 1).padStart(2, '0')
+let ano = data.getFullYear()
+data = dia + '/' + mes + '/' + ano
+
+const representanteAssinatura = document.querySelector('#representante-assinatura')
+
+btnPdf.addEventListener("click", () => {
+
+  document.querySelector('#nome-fantasia').textContent = nomeFantasia.value
+  document.querySelector('#empresa-contratante').textContent = empresaContratante.value
+  document.querySelector('#cnpj').textContent = cnpj.value
+  document.querySelector('#inscricao-estadual').textContent = inscricaoEstadual.value
+  document.querySelector('#rua').textContent = rua.value
+  document.querySelector('#numero-rua').textContent = numeroRua.value
+  document.querySelector('#bairro').textContent = bairro.value
+  document.querySelector('#cidade').textContent = cidade.value
+  document.querySelector('#cep').textContent = cep.value
+  document.querySelector('#representante').textContent = representante.value
+  document.querySelector('#cidadania').textContent = cidadania.value
+  document.querySelector('#estado-civil').textContent = estadoCivil.value
+  document.querySelector('#profissao').textContent = Profissao.value
+  document.querySelector('#cpf').textContent = cpf.value
+  document.querySelector('#rg').textContent = rg.value
+  document.querySelector('#data').textContent = data
+  document.querySelector('#representante-assinatura').textContent = representante.value
+
+  geraPdf()
+})
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const btnPdf = document.querySelector('#generate-pdf');
-const headerSigesis = document.querySelector('#headerSigesis');
-const footerSigesis = document.querySelector('#footerSigesis');
+const headerSigesis = document.querySelector('#headerSigesis')
+const footerSigesis = document.querySelector('#footerSigesis')
 
-btnPdf.addEventListener("click", () => {
+function geraPdf() {
   let texto = document.querySelector('#content');
-
+  texto.style.display = 'block';
+  
   const options = {
     margin: [50, 50, 50, 50],
     filename: "teste.pdf",
@@ -12,18 +12,20 @@ btnPdf.addEventListener("click", () => {
     jsPDF: { unit: "pt", format: "a4", orientation: "portrait" },
     pagebreak: { mode: 'avoid-all', before: '#page2el' },
     pageBreak: { mode: 'legacy' },
-  };
+  }
 
   html2pdf().from(texto).set(options).toPdf().get('pdf').then(function (pdf) {
-    let totalPages = pdf.internal.getNumberOfPages();
-    let pageWidth = pdf.internal.pageSize.getWidth();
-    let pageHeight = pdf.internal.pageSize.getHeight();
-    let imageWidth = 100;
-    let imageX = (pageWidth - imageWidth) / 2;
+    let totalPages = pdf.internal.getNumberOfPages()
+    let pageWidth = pdf.internal.pageSize.getWidth()
+    let pageHeight = pdf.internal.pageSize.getHeight()
+    let imageWidth = 100
+    let imageX = (pageWidth - imageWidth) / 2
     for (let i = 1; i <= totalPages; i++) {
-      pdf.setPage(i);
-      pdf.addImage(headerSigesis.src, 'JPEG', (imageX = (pageWidth - imageWidth)/2), 10, imageWidth, 30);
-      pdf.addImage(footerSigesis.src, 'JPEG', (imageX = (pageWidth - 200)/2), pageHeight - 50, 200, 20);
+      pdf.setPage(i)
+      pdf.addImage(headerSigesis.src, 'JPEG', (imageX = (pageWidth - imageWidth)/2), 10, imageWidth, 30)
+      pdf.addImage(footerSigesis.src, 'JPEG', (imageX = (pageWidth - 200)/2), pageHeight - 50, 200, 20)
     }
-  }).save();
-});
+  }).save().then(() => {
+    texto.style.display = 'none';
+  })
+}
